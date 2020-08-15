6877147d40dd5c7a6975e7ef51a9793d41e0e989
daniel campo <campo_daniel94@yahoo.com>
2020-08-15 03:31:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,2 @@
 class Dog
-
- def initialize(name)
-  @name = name
 end
-  
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-
-end
-
-fido = Dog.new
-fido.name = "fido"
-fido.name
